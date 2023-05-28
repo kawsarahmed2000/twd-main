c746290e78210e01375638bfd4b16a7c2efc4342
kawsarahmed2000 <96857085+kawsarahmed2000@users.noreply.github.com>
2023-05-28 22:43:30 +0530

    otp verification 2

--- assets/js/otp-verification.js
+++ assets/js/otp-verification.js
@@ -2,20 +2,9 @@ const codes = document.querySelectorAll('.code')
 
 codes[0].focus()
 
-// codes.forEach((code, idx) => {
-//     code.addEventListener('keydown', (e) => {
-//         if(e.key >= 0 && e.key <=9) {
-//             codes[idx].value = ''
-//             setTimeout(() => codes[idx + 1].focus(), 10)
-//         } else if(e.key === 'Backspace') {
-//             setTimeout(() => codes[idx - 1].focus(), 10)
-//         }
-//     })
-// })
-// localStorage.setItem("email","daspurab80@gmail.com")
 var email = localStorage.getItem("email");
-document.getElementById("labels").textContent = `We emailed you the six digit code to ${email} <br />
-      Enter the code below to confirm your email address`
+document.getElementById("labels").textContent = `We emailed you the six digit code to ${email}`
+document.getElementById("labels2").textContent = `Enter the code below to confirm your email address`
 function otpVerification(){
   var type = localStorage.getItem("type")
   var otp = document.getElementById("otp").value;
